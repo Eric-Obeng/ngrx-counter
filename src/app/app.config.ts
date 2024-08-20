b413f35528c774ba47a8d2f9aa66a0af78b07831
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-20 17:09:12 +0000

    jest setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,14 @@ import { routes } from './app.routes';
 import { provideStore, provideState } from '@ngrx/store';
 
 import { counterReducer } from './state/counter.reducer';
+import { CounterEffects } from './state/counter.effects';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideStore(),
+    provideStore({counter: counterReducer}),
     provideState({ name: 'counter', reducer: counterReducer }),
+    provideEffects([CounterEffects])
   ],
 };
